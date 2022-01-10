7c9bae3e60e4f1d1f3541ec044bc3deb5e65a5b0
Troels21 <Troels.e.kiib@gmail.com>
2022-01-10 09:09:23 +0100

    Python kode i folder

---
+++ Python/BaseLineWander.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 12:57:08 2022
+@author: rajaabengad
+"""
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+import wfdb
+
+
+fs=500
+
+ 
+Ts = 1/fs
+ 
+t = np.arange(0,2000*Ts,Ts)
+
+# Indlæs EKG signalet (råt, ufiltreret) samt tilhørende info
+signals, info = wfdb.rdsamp('rec_1', channels=[0, 1], 
+                              sampfrom=0, sampto=2000)
+
+# 0 er råt signal, 1 er filtreret signal
+ecg0 = signals[:,0]
+ecg1 = signals[:,1]
+
+# Plot signal i tidsdomæne
+plt.plot(t,ecg0)
+plt.title('Rå signal i tidsdomænet')
+plt.xlabel('tid (s)')
+plt.ylabel('Amplitude (mV)')
+plt.show()
+
+# Plot signal i frekvensdomæne
+plt.magnitude_spectrum(ecg0,Fs=500)
+plt.show()
+
+#  Lavpas filter
+# #FIR, highpass, zero phase, forward-backward filtrering, cutoff=0.5Hz
+filter = signal.firwin(numtaps=301, fs=500, cutoff=0.6, pass_zero=False)
+filtreretSignal = signal.filtfilt(filter,1,ecg0)
+plt.plot(t,filtreretSignal, color='orange')
+plt.plot(t,ecg0, color='blue')
+plt.xlabel('tid (s)')
+plt.ylabel('Amplitude (mV)')
+plt.show()
+
+#blå= rå ekg orange= ekg gennem highpass filter 
+
+
+# Filtreret signal
+plt.magnitude_spectrum(filtreretSignal,Fs=500, color='orange')
+plt.ylim([0,0.04])
+
+# Ufiltreret signal
+#plt.magnitude_spectrum(ecg0,Fs=500, color='green')
+
+plt.show()
---
+++ Python/Python Post request.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 10 08:57:19 2022
+
+@author: Spille
+"""
+
+# importing the requests library
+import requests
+import json
+  
+# OBBBBSSSSS     Tomcat Serveren skal PT være tændt.
+
+endpoint = "http://localhost:8080/SemesterProjekt_3_war/data/ekg"
+
+
+cpr= "1234567890" #CPR
+dsparray = [1,2,3,4,5,6,7,8,9,23,32,41,252,3,562,34,421,3,0] #Dsp plot array
+Headers={'Authorization': 'Bearer hemmeliglogin', #Loginkode
+         'Identifier': cpr} 
+
+  
+# Post request, med endpoint, dataen og headers til genkendelse og sikkerhed.
+r = requests.post(endpoint,  data = json.dumps(dsparray), headers = Headers )
+  
+#Modtage responbesked 
+Response = r.text
+print(Response)
